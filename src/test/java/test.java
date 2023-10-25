c2e0179214d62a57b43f926d6c8a57054f4eb7b4
Mrx97-X <56883644+Mrx97-X@users.noreply.github.com>
2023-10-25 10:52:54 +0800

    Initial commit

--- src/test/java/test.java
+++
@@ -1,6 +0,0 @@
-public class test {
-    public static void main(String[] args) {
-        System.out.println(System.getProperty("java.library.path"));
-        System.out.println(System.getProperty("jni.linker.flags"));
-    }
-}
